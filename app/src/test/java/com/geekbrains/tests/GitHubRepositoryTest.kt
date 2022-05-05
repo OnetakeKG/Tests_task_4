d509117c6d163baa526932acc5cdd17144f45e46
user <gufeld1488@gmail.com>
2022-05-05 11:48:26 +0300

    Initial commit

---
+++ app/src/test/java/com/geekbrains/tests/GitHubRepositoryTest.kt
@@ -0,0 +1,109 @@
+package com.geekbrains.tests
+
+import com.geekbrains.tests.model.SearchResponse
+import com.geekbrains.tests.repository.GitHubApi
+import com.geekbrains.tests.repository.GitHubRepository
+import com.geekbrains.tests.repository.RepositoryCallback
+import okhttp3.Request
+import okio.Timeout
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class GitHubRepositoryTest {
+
+    private lateinit var repository: GitHubRepository
+
+    @Mock
+    private lateinit var gitHubApi: GitHubApi
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        repository = GitHubRepository(gitHubApi)
+    }
+
+    @Test
+    fun searchGithub_Test() {
+        val searchQuery = "some query"
+        val call = mock(Call::class.java) as Call<SearchResponse?>
+
+        `when`(gitHubApi.searchGithub(searchQuery)).thenReturn(call)
+        repository.searchGithub(searchQuery, mock(RepositoryCallback::class.java))
+        verify(gitHubApi, times(1)).searchGithub(searchQuery)
+    }
+
+    @Test
+    fun searchGithub_TestCallback() {
+        val searchQuery = "some query"
+        val response = mock(Response::class.java) as Response<SearchResponse?>
+        val RepositoryCallback = mock(RepositoryCallback::class.java)
+
+        val call = object : Call<SearchResponse?> {
+            override fun enqueue(callback: Callback<SearchResponse?>) {
+                callback.onResponse(this, response)
+                callback.onFailure(this, Throwable())
+            }
+
+            override fun clone(): Call<SearchResponse?> {
+                TODO("Not yet implemented")
+            }
+
+            override fun execute(): Response<SearchResponse?> {
+                TODO("Not yet implemented")
+            }
+
+            override fun isExecuted(): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun cancel() {
+                TODO("Not yet implemented")
+            }
+
+            override fun isCanceled(): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun request(): Request {
+                TODO("Not yet implemented")
+            }
+
+            override fun timeout(): Timeout {
+                TODO("Not yet implemented")
+            }
+        }
+
+        `when`(gitHubApi.searchGithub(searchQuery)).thenReturn(call)
+        repository.searchGithub(searchQuery, RepositoryCallback)
+
+        verify(RepositoryCallback, times(1)).handleGitHubResponse(response)
+        verify(RepositoryCallback, times(1)).handleGitHubError()
+    }
+
+    @Test
+    fun searchGithub_TestCallback_WithMock() {
+        val searchQuery = "some query"
+        val call = mock(Call::class.java) as Call<SearchResponse?>
+        val callBack = mock(Callback::class.java) as Callback<SearchResponse?>
+        val repositoryCallback = mock(RepositoryCallback::class.java)
+        val response = mock(Response::class.java) as Response<SearchResponse?>
+
+        `when`(gitHubApi.searchGithub(searchQuery)).thenReturn(call)
+        `when`(call.enqueue(any())).then {
+            callBack.onResponse(any(), any())
+        }
+        `when`(callBack.onResponse(any(), any())).then {
+            repositoryCallback.handleGitHubResponse(response)
+        }
+
+        repository.searchGithub(searchQuery, repositoryCallback)
+
+        verify(repositoryCallback, times(1)).handleGitHubResponse(response)
+    }
+}
